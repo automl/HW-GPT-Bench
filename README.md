b6427d11b6c362c81ecae1f674035e39552ac95d
arberzela <arber.zela21@gmail.com>
2024-05-28 17:24:20 +0200

    added setup.py and changed gitignore

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HW-Aware-LLM-Bench
 Repository for Hardware Aware LLM Benchmark
+
+To install in editable mode (-e) run:
+
+```sh
+$ git clone https://github.com/automl/HW-Aware-LLM-Bench
+$ cd HW-Aware-LLM-Bench
+$ pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# Read the contents of requirements.txt file
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
+setuptools.setup(
+    name="hwgptbench",
+    version="0.1",
+    author="AutoML Freiburg",
+    author_email="sukthank@cs.uni-freiburg.de",
+    description=("A surrogate benchmark for multi-objective neural architecture search."),
+    long_description=long_description,
+    url="https://github.com/automl/HW-Aware-LLM-Bench",
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    keywords="machine learning"
+             "surrogate benchmark gpt hardware-aware NAS deep learning",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+	python_requires='>=3.6',
+    platforms=['Linux'],
+    install_requires=required_packages,
+    include_package_data=True
+    #extras_require=optional_requirements
+)
